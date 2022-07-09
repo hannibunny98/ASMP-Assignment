8cccc76561aa8cb55ff333d1105fd7b5515b1d69
sbkueh <sebastian.kuehn19@gmail.com>
2022-07-09 13:59:00 +0200

    array tranfer vector plot

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+from array import array
 from src.sensor import ArraySensor
 from src.beamformer import ConventionalBeamformer, CaponBeamformer, WierdBeamformer, Beamformer
 import numpy as np
-# import util.plot as plt
-
+import util.plot as plt2
 import matplotlib.pyplot as plt
 
 
@@ -11,10 +11,10 @@ def get_timesteps(samplerate, channels):
 
 
 if __name__ == '__main__':
-    array_sensor = ArraySensor('./Measurements/CrowsNest.csv', './Measurements/measurements1.wav', 340.29)
-    beamformer = ConventionalBeamformer(array_sensor)
+    array_sensor = ArraySensor('./Measurements/CrowsNest.csv', './Measurements/measurements1.wav', 343)
+    # beamformer = ConventionalBeamformer(array_sensor)
 
-    n = 100
+    n = 200
     wh = 2 * n + 1
 
     d = np.mgrid[:wh, :wh].T.reshape(-1, 2) / n - 1
@@ -22,19 +22,20 @@ if __name__ == '__main__':
 
     m = np.where(np.square(d).sum(axis=1) <= 1)
 
-    d_up = np.c_[d, np.emath.sqrt(1 - np.square(d).sum(axis=1))]
+    d_down = np.c_[d, -np.emath.sqrt(1 - np.square(d).sum(axis=1))]
 
-    b = beamformer.spatial_power_spectrum(d_up[m])
+    # b = beamformer.spatial_power_spectrum(d_down[m])
+    b = np.angle(array_sensor.A(d[m], 400)[15])
     # b = np.abs(array_sensor.AF(np.array([[0, 0]]), d[m], 500))
 
     i[m] = b
 
     fig, ax = plt.subplots()
-    im = ax.imshow(i.reshape(wh, wh))
+    im = ax.imshow(i.reshape(wh, wh)[::-1, ::-1])
     plt.show()
 
-    exit()
+    # exit()
 
-    Time = get_timesteps(array_sensor.samplerate, array_sensor.measurments)
-    plt.plot_channels(array_sensor.measurments, Time)
-    plt.plot_channel_spectogram(array_sensor.measurments.T[0], array_sensor.samplerate)
+    # Time = get_timesteps(array_sensor.samplerate, array_sensor.measurments)
+    # plt2.plot_channels(array_sensor.measurments, Time)
+    # plt2.plot_channel_spectogram(array_sensor.measurments.T[0], array_sensor.samplerate)
