ea81f311cc53cde4cc3ae03e037044f3278b5313
sbkueh <sebastian.kuehn19@gmail.com>
2022-07-10 14:02:45 +0200

    added array factor plot function

--- util/plot.py
+++ util/plot.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
+from src.sensor import ArraySensor
 
 
 def plot_channels(channels, Time):
@@ -18,4 +21,30 @@ def plot_channel_spectogram(channel, framerate):
     plt.title('Spectogram of channel 1')
     plt.xlabel('Time [s]')
     plt.ylabel('Frequency [Hz]')
+    plt.show()
+
+
+def plot_array_transfer_vector(array_sensor: ArraySensor, frequency: float, n: int, w: int = 1):
+    images = np.full(((n + 1)**2, array_sensor.positions.shape[0]), np.nan)
+
+    d = np.mgrid[:n + 1, :n + 1].T.reshape(-1, 2) * 2 / n - 1
+    m = np.where(np.square(d).sum(axis=1) <= 1)
+
+    A = array_sensor.A(np.c_[d[m], w * np.sqrt(1 - np.square(d[m]).sum(axis=1))], frequency)
+
+    images[m] = np.angle(A).T
+    images = images.T.reshape((-1, n + 1, n + 1))
+
+    x = np.sqrt(images.shape[0]).astype(int)
+    y = images.shape[0] // x + images.shape[0] % x
+
+    fig, ax = plt.subplots(x, y, tight_layout=False, sharey=True, sharex=True)
+
+    for axes, image in zip(ax.reshape(-1), images):
+        img = axes.imshow(image[::-1, ::-1], extent=[-1, 1, -1, 1], cmap='hsv')
+        axes.set_xticks([-1, 0, 1])
+        axes.set_yticks([-1, 0, 1])
+
+    fig.colorbar(img, ax=ax, location='right')
+
     plt.show()
